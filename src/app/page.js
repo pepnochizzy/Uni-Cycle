54e558b4d2cd6a44e4c392d4563c67e5d50a55f1
pepnochizzy <laragray@live.co.uk>
2026-02-16 13:18:57 +0000

    chore: adding buttons back to homepage for testing

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,16 @@
 import SubmitForm from "@/components/SubmitForm";
+import { SignInButton, SignUpButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <h2>Homepage</h2>
       <SubmitForm />
+      {/* just put bg color and flex for our visibility, remove when styling */}
+      <div className="bg-[#77AF9C] flex place-content-around">
+        <SignUpButton />
+        <SignInButton />
+      </div>
     </div>
   );
 }
-
-
-

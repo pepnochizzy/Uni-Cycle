59cd0b2caed636d338faad1ceb5f4335ebdf51e8
ULRIKE KUNZE <ULRIKE.E.KUNZE@GMAIL.COM>
2026-02-17 14:04:54 +0000

    feat: reactions and comment both now on marketplace and individual item

--- src/components/Likes.jsx
+++ src/components/Likes.jsx
@@ -1,4 +1,4 @@
-import { db } from "@/utils/dbConnection";
+import { db } from "@/utils/dbConnections";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { revalidatePath } from "next/cache";

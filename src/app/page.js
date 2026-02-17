2aaf4a0d437a3288c1a8579e8fc6748ab7d78fca
TSBIceLizard <generalkajgue@hotmail.com>
2026-02-17 14:15:02 +0000

    feat: Added stuff to homepage including animations and make sure to npm i again

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,31 @@
 // root / homepage
 import SubmitForm from "@/components/SubmitForm";
-import { UserButton, SignInButton, SignUpButton, SignedIn, SignedOut } from "@clerk/nextjs";
+import AboutUsOne from "@/components/AboutUsOne";
+import {
+  UserButton,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+} from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <h2>Homepage</h2>
-      <SignedOut>
-        <div className="sign-buttons">
-          <SignUpButton className="button" />
-          <SignInButton className="button" />
-        </div>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <SubmitForm />
-    </div>
+    <>
+      <div>
+        <h2>Homepage</h2>
+        <SignedOut>
+          <div className="sign-buttons">
+            <SignUpButton className="button" />
+            <SignInButton className="button" />
+          </div>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <SubmitForm />
+      </div>
+      <AboutUsOne />
+    </>
   );
 }
---
+++ src/components/AboutUsOne.jsx
@@ -0,0 +1,44 @@
+import AboutUsStyles from "./AboutUs.module.css";
+import EnterAnimate from "./EnterAnimate";
+
+export default function AboutUsOne() {
+  return (
+    <>
+      <EnterAnimate>
+        <div className={AboutUsStyles.about_us_1}>
+          <h2>Uni-Cycle</h2>
+          <h3>About us:</h3>
+          <p>
+            Have you ever set up your uni dorm room for your studies and either
+            had too many items you didn&apos;t need ... or needed items that you
+            didn&apos;t have?
+          </p>
+        </div>
+      </EnterAnimate>
+      <EnterAnimate>
+        <div className={AboutUsStyles.about_us_1}>
+          <p>Maybe consider our marketplace service</p>
+          <h3>... Uni-Cycle ...</h3>
+        </div>
+      </EnterAnimate>
+      <EnterAnimate>
+        <div className={AboutUsStyles.about_us_1}>
+          <p>
+            ... Our app gives students the capability to manage their inventory
+            by establishing an easy to use platform to facilitate the exchange
+            of items!
+          </p>
+          <p>... Remember that geography book that was the wrong one?</p>
+          <p>
+            ... Remember that clumbersome chair that was too big for your room?
+          </p>
+          <p>... Remember when you misplaced your utensils?</p>
+          <p>
+            ... Remember when your assignment was coming up, but you had a
+            knowledge gap in a very specific topic?
+          </p>
+        </div>
+      </EnterAnimate>
+    </>
+  );
+}

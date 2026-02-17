a355419fe63456bbce4da656551fdfe2a11a4190
TSBIceLizard <generalkajgue@hotmail.com>
2026-02-17 10:33:37 +0000

    feat: marketplace listings mapped

--- src/app/marketplace/page.js
+++ src/app/marketplace/page.js
@@ -1,5 +1,35 @@
-export default function MarketPlace() {
-    return (
-        <h2>Maretplace</h2>
-    );
+import { db } from "@/utils/dbConnections";
+import Link from "next/link";
+import Image from "next/image";
+
+export default async function MarketPlace() {
+  const marketItems = await db.query(`SELECT * FROM uni_posts`);
+  //   const parsedMarketItems = marketItems.rows;
+
+  console.log(marketItems);
+
+  return (
+    <>
+      <h2>Maretplace</h2>
+      <div>
+        {marketItems.rows.map((marketItem) => {
+          return (
+            <Link key={marketItem.id} href={`/marketplace/${marketItem.id}`}>
+              <article>
+                <Image
+                  src={marketItem.image}
+                  alt={marketItem.post}
+                  width={256}
+                  height={256}
+                />
+                <p>{marketItem.post}</p>
+                <p>{marketItem.category}</p>
+                <p>{marketItem.created_at.toLocaleString()}</p>
+              </article>
+            </Link>
+          );
+        })}
+      </div>
+    </>
+  );
 }

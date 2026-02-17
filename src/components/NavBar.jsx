d66a8b9ea162ba3358da941e44db71dda76ac6de
ULRIKE KUNZE <ULRIKE.E.KUNZE@GMAIL.COM>
2026-02-17 15:50:12 +0000

    fix to sign and sign out navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,33 +4,52 @@ import { auth } from "@clerk/nextjs/server";
 import { db } from "@/utils/dbConnections";
 import Image from "next/image";
 import logo from "@/../public/assets/logo.png";
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+  SignOutButton,
+} from "@clerk/nextjs";
 
 //homepage, market  place, profile page/sign out
 export default async function Navbar() {
   const { userId } = await auth();
+  let user = null;
   console.log({ userId });
-  const usernameQuery = await db.query(
-    `SELECT username FROM uni_users where clerk_id = $1`,
-    [userId],
-  );
-  const user = usernameQuery.rows[0];
+
+  if (userId) {
+    const { rows } = await db.query(
+      `SELECT username FROM uni_users where clerk_id = $1`,
+      [userId],
+    );
+    user = rows[0] || null;
+  }
+
   return (
     // <nav className="flex place-content-between">
     <nav>
       <Link href={"/"}>
         <Image src={logo} alt="Uni-cycle logo" height={80} />
       </Link>
+      <SignedOut>
+        <Link href={"/"}>Home</Link>&nbsp;|&nbsp;
+        <SignInButton className="button" />
+        <SignUpButton className="button" />
+      </SignedOut>
+      <SignedIn>
+        {user && <Link href={`/studentarea/${user.username}`}>Profile</Link>}
+        <Link href={"/marketplace"}>Marketplace</Link>&nbsp;|&nbsp;
+        <SignOutButton className="button"></SignOutButton>
+      </SignedIn>
+
       {/* <div className="flex w-60"> */}
       <div>
         {/* <Link href={"/"} className="pr-4 border-r-2 border-r-[#77AF9C]"> */}
-        <Link href={"/"}>Home</Link>&nbsp;|&nbsp;
+
         {/* <Link href={"/marketplace"} className="pl-4 pr-4 border-r-2 border-r-[#77AF9C]"> */}
-        <Link href={"/marketplace"}>Marketplace</Link>&nbsp;|&nbsp;
         {/* <div className="pl-4"> */}
-        <Link href={`/studentarea/${user.username}`}>Profile</Link>
-        <div className="dropdown">
-          <NavDropdown />
-        </div>
       </div>
     </nav>
   );

376fc64124420db2000f237cb92d13efea4f29c0
Andrew Birch <ab8218-cp@yahoo.com>
2026-02-18 10:04:20 +0000

    FEAT: footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <>
+            <footer>
+                <hr />
+                <p>Design: EggCentric</p>
+                <p>2026</p>
+            </footer>
+        </>
+    )
+}

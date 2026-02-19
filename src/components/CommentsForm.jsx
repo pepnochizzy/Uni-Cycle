8d74d592818dfeadef3ef7283927d091c8c14bec
pepnochizzy <laragray@live.co.uk>
2026-02-19 11:17:49 +0000

    feat: comment tree working with styling

--- src/components/CommentsForm.jsx
+++ src/components/CommentsForm.jsx
@@ -1,25 +1,32 @@
 "use client";
 
 import { useState } from "react";
+import style from "@/styles/comments.module.css";
 
-export function CommentForm({ postId, parentId, action }) {
+export function CommentForm({ postId, parentCommentId, action }) {
   const [isOpen, setOpen] = useState(false);
 
   return (
-    <>
-      <button onClick={() => setOpen(!isOpen)}>
+    <div className={style.replyDiv}>
+      <button onClick={() => setOpen(!isOpen)} className={style.buttonSmall}>
         {isOpen ? "Close" : "Reply"}
       </button>
       {isOpen ? (
         <>
           <form action={action}>
-            <input type="hidden" name="parent_Id" value={parentId} />
+            <input type="hidden" name="parent_Id" value={parentCommentId} />
             <input type="hidden" name="post_id" value={postId} />
-            <textarea name="comment" placeholder="Reply to this comment" />
-            <button type="submit">Post</button>
+            <textarea
+              name="comment"
+              placeholder="Reply to this comment"
+              className={style.textArea}
+            />
+            <button type="submit" className={style.buttonSmall}>
+              Post
+            </button>
           </form>
         </>
       ) : null}
-    </>
+    </div>
   );
 }

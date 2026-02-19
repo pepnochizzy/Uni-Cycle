dadc3db3759b782e744bb4a46351dac93fa9af6b
arronburgess <generalkajgue@hotmail.com>
2026-02-19 11:52:42 +0000

    feat: started a modal component for adding items2

--- src/components/SubmitItemModal.jsx
+++ src/components/SubmitItemModal.jsx
@@ -1,25 +1,30 @@
 "use client";
+import { routerServerGlobal } from "next/dist/server/lib/router-utils/router-server-context";
 import SubmitForm from "./SubmitForm";
+import { useRouter } from "next/router";
 
 //! This way of doing a modal might be super antiquated, haven't used getElementById since before week 6!
 export default function SubmitItemModal() {
-  const submitItemModal = document.getElementById("submititemmodal");
+  //! This antiquated way didn't work with next !
+  //   const submitItemModal = document.getElementById("submititemmodal");
 
-  const submitItemButton = document.getElementById("additemformmodal");
+  //   const submitItemButton = document.getElementById("additemformmodal");
 
-  const closeModal = document.getElementById("closemodal");
+  //   const closeModal = document.getElementById("closemodal");
 
-  submitItemButton.onclick = function () {
-    submitItemModal.style.display = "block";
-  };
+  //   submitItemButton.onclick = function () {
+  //     submitItemModal.style.display = "block";
+  //   };
 
-  closeModal.onclick = function () {
-    submitItemModal.style.display = "none";
-  };
+  //   closeModal.onclick = function () {
+  //     submitItemModal.style.display = "none";
+  //   };
 
   return (
     <>
-      <button id="additemformmodal">+ Add Marketplace Item +</button>
+      <button onClick={routerServerGlobal.back}>
+        + Add Marketplace Item +
+      </button>
       <div id="submititemmodal">
         <span id="closemodal">X</span>
         <SubmitForm />

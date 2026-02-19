9d6633a1cc8b56fd3b94e922eb71c2ecb0d45f55
pepnochizzy <laragray@live.co.uk>
2026-02-19 13:26:35 +0000

    feat: adding bold to titles and bullet points to home

--- src/app/marketplace/page.js
+++ src/app/marketplace/page.js
@@ -65,7 +65,7 @@ export default async function MarketPlace(props) {
 
   return (
     <>
-      <h1>Marketplace</h1>
+      <h1 className="font-bold">Marketplace</h1>
       <Filters searchParams={searchParams} />
       <div>
         <SubmitForm />
